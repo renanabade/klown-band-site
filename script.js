0057805f09c90dec179ae446a752d201130a9f77
Renan <134206626+renanabade@users.noreply.github.com>
2024-09-16 20:32:44 -0300

    adicionando easter egg e guardando arquivos em pastas

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const floatingIcon = document.getElementById('play-sound');
     const sound = document.getElementById('sound');
 
     floatingIcon.addEventListener('click', function() {
         sound.play();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const easterEggIcon = document.getElementById('easteregg-icon');
+    const easterEggSound = document.getElementById('easteregg-sound');
+
+    easterEggIcon.addEventListener('click', function() {
+        easterEggSound.play();
     });
 });
